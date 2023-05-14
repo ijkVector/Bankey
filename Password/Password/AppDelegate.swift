0aeb24c1f2834aa8e1fa23af8a0c3b1ff8b1b86b
ijkVector <johnsmith28122000@gmail.com>
2023-05-14 07:55:07 +0300

    feat: create new password project

---
+++ Password/Password/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Password
+//
+//  Created by Иван Дроботов on 14.05.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+        
+    }
+
+}
+
---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Иван Дроботов on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
